f1cb4d9554c176e966e4d0dff1f361aa4e69585d
Laxman Maharjan <email@mlaxman.com.np>
2017-11-03 19:57:53 +0545

    urlfixes

--- include/config.php
+++ include/config.php
@@ -1,14 +1,14 @@
-<?php 
-$site_url='http://localhost/oleCDT/';
+<?php
+$site_url = 'http://localhost:8000/travelguide/';
 $host = 'localhost';
 $dbname = 'dppms_fieldguide';
 $username = 'dppms';
 $password = 'dfHGf$3987Jgf';
 
-date_default_timezone_set("Asia/Kolkata");  
+date_default_timezone_set("Asia/Kolkata");
 
 try {
-  $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+  $conn = new PDO("mysql:host=$host; dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   //echo "Connected successfully";
