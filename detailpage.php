167e9f60f597f970984c219884cbe2c6f9c20652
Laxman Maharjan <email@mlaxman.com.np>
2017-11-04 17:01:37 +0545

    [#6] publisher review submit and show

--- detailpage.php
+++ detailpage.php
@@ -12,42 +12,41 @@ $query->bindParam(':id',$id,PDO::PARAM_STR);
 $query->execute();
 $row = $query->fetch(PDO::FETCH_ASSOC);
 ?>
-    
 
 <section id="contact" class=" background-img-uplod" style="padding: 156px 0 5%;">
-	<div class="container">
-		<div class="row">
+  <div class="container">
+    <div class="row">
       <div class="col-sm-12 jal mar-left-12 pad-ouar new-img">
-				<div class="manu" data-wow-delay=".6s" data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.6s; animation-name: fadeInRight;">
-					<a href="<?php echo $site_url;?>searching"><p class=" jal-2 jal-4"><span><b>Return to Directory</b></span><br>                              
-          </p></a>					
-				</div>
-				<!-- /.intro-block end -->
-			</div>
-			<!--	<div class="col-sm-12 jal mar-left-12 pad-ouar new-img pull-right">
-				<div class="manu" data-wow-delay=".6s" data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.6s; animation-name: fadeInRight;">
-					<a href="<?php echo $site_url;?>update_organization/<?php echo base64_encode($id);?>"><p class=" jal-2 jal-4"><span><b>Update Profile</b></span><br>                            
-          </p></a>					
-				</div>	
-			</div>-->
+        <div class="manu" data-wow-delay=".6s" data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.6s; animation-name: fadeInRight;">
+          <a href="searching"><p class=" jal-2 jal-4"><span><b>Return to Directory</b></span><br>
+          </p></a>
+        </div>
+        <!-- /.intro-block end -->
+      </div>
+      <!-- <div class="col-sm-12 jal mar-left-12 pad-ouar new-img pull-right">
+        <div class="manu" data-wow-delay=".6s" data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.6s; animation-name: fadeInRight;">
+          <a href="<?php echo $site_url;?>update_organization/<?php echo base64_encode($id);?>"><p class=" jal-2 jal-4"><span><b>Update Profile</b></span><br>
+          </p></a>
+        </div>
+      </div>-->
       <div class="text-pera last">
-				<div class="lastupdate">Last Updated On  <?php echo date('d-M-Y',strtotime($row['last_update'])); ?></div>
-				<h1><?php echo $row['name'];?></h1>
-				
-				<div class="col-sm-9">
-				  <p><b style="color:#000;">Mission:</b><?php echo $row['mission'];?></p>
-				  <p>Year Founded: <?php echo $row['establish_year']; ?>  •   <?php echo $row['o_phone'];?>   •   <?php echo $row['o_email'];?>  •   <a href="<?php echo $row['weblink'];?>"><?php echo $row['weblink'];?></a></p>
-				</div>
-						   
-				<div class="col-sm-3">
+        <div class="lastupdate">Last Updated On <?php echo date('d-M-Y',strtotime($row['last_update'])); ?></div>
+        <h1><?php echo $row['name'];?></h1>
+        
+        <div class="col-sm-9">
+          <p><b style="color:#000;">Mission:</b><?php echo $row['mission'];?></p>
+          <p>Year Founded: <?php echo $row['establish_year']; ?> • <?php echo $row['o_phone'];?> • <?php echo $row['o_email'];?>  •   <a href="<?php echo $row['weblink'];?>"><?php echo $row['weblink'];?></a></p>
+        </div>
+               
+        <div class="col-sm-3">
         <?php if($row['logo']!=''){ ?>
-          <img src="<?php echo $site_url;?>img/<?php echo $row['logo'];?>" height="150px" width="150px">
+          <img src="img/<?php echo $row['logo'];?>" height="150px" width="150px">
         <?php } else { ?>
-          <img src="<?php echo $site_url;?>img/ole_logo.png" height="150px" width="150px">
+          <img src="img/ole_logo.png" height="150px" width="150px">
         <?php } ?>
       </div>
 
-      <div class="w-line"></div>					                            
+      <div class="w-line"></div>
 
       <div class="col-sm-12">
         <div class="new-1-pera">
@@ -55,8 +54,8 @@ $row = $query->fetch(PDO::FETCH_ASSOC);
           <p><?php echo $row['work_type'];?></p>
         </div> 
       </div>
-					
-			<div class="w-line-2"></div>
+
+      <div class="w-line-2"></div>
 
       <div class="col-sm-12">
         <div class="new-1-pera">
@@ -64,10 +63,10 @@ $row = $query->fetch(PDO::FETCH_ASSOC);
           <p><?php echo $row['achievements'];?></p>
         </div> 
       </div>
-					
-			<div class="w-line-2"></div>
 
-			<div class="col-sm-12">
+      <div class="w-line-2"></div>
+
+      <div class="col-sm-12">
         <div class="new-1-pera">
           <h1>Your Entry Points</h1>
           <p class="font_9"><span style="color:#666666;">Attend the Brennan Center’s&nbsp;</span><span style="font-weight:bold;"><a href="#" target="_blank" data-content="#" data-type="external"><span style="color:#0957a2;">events</span></a></span><span style="color:#666666;">&nbsp;and explore its detailed&nbsp;</span><span style="font-weight:bold;"><a href="#" target="_blank" data-content="#" data-type="external"><span style="color:#0957a2;">policy proposals</span></a></span><span style="color:#666666;">,&nbsp;</span><span style="font-weight:bold;"><a href="#" target="_blank" data-content="#" data-type="external"><span style="color:#0957a2;">expert analysis</span></a></span><span style="color:#666666;">, and easy-to-read&nbsp;</span><span style="font-weight:bold;"><a href="http://www.brennancenter.org/press" target="_blank" data-content="#" data-type="external"><span style="color:#0957a2;">articles</span></a></span><span style="color:#666666;">&nbsp;online. Want to get more involved? Apply for one of the Brennan Center’s many&nbsp;</span><span style="font-weight:bold;"><a href="#" 
@@ -78,58 +77,36 @@ $row = $query->fetch(PDO::FETCH_ASSOC);
           </a></span><span style="color:#666666;">&nbsp;with the Center on litigation, policy research, 
           and public advocacy. While based in New York City, the Brennan Center works with people across
            the country and in many different fields.</span></p>
-        </div> 
+        </div>
       </div>
-					
-			<div class="col-sm-12">
-			  <div class="piy-p">
-					<div class="col-sm-6 butten">
-					  <div class="star ">
+          
+      <div class="col-sm-12">
+        <div class="piy-p">
+          <div class="col-sm-6 butten">
+            <div class="star ">
               <p>Rate this item</p>
-              <?php 
+              <?php
               $trat='';
+              $rcnt[1] = $rcnt[2] = $rcnt[3] = $rcnt[4] = $rcnt[5] = 0;
               $rating = $conn->prepare("SELECT * FROM `rating` WHERE oid=:id");
               $rating->execute(array(':id' => $id));
               $count2 = $rating->rowCount();
               $r_row = $rating->fetchAll(PDO::FETCH_ASSOC);
-              if($count2 >0)
+              if($count2 > 0)
               {
                 foreach($r_row as $r_row1)
                 {
+                  $rcnt[$r_row1['rating']]++;
                   $trat+=$r_row1['rating'];
                 }
                 $rscore=floor($trat/$count2);
-	              if($rscore==1){ ?>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:black"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:black"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:black"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:black"></i>
-                <?php } else if($rscore==2) { ?>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:black"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:black"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:black"></i>
-                <?php } else if($rscore==3) { ?>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:black"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:black"></i>
-                <?php } else if($rscore==4) { ?>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:black"></i>
-                <?php } else if($rscore == 5) { ?>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                  <i class="fa fa-star" style="font-size:30px;color:yellow"></i>
-                <?php } else{}
+                for($i=1; $i<=5; $i++) {
+                  if($rscore >= $i) {
+                    echo '<i class="fa fa-star" style="font-size:30px;color:yellow"></i>';
+                  } else {
+                    echo '<i class="fa fa-star" style="font-size:30px;color:black"></i>';
+                  }
+                }
               } ?>
               <?php include('include/reviewandrating.php');
               if(isset($_SESSION['id']))
@@ -139,38 +116,38 @@ $row = $query->fetch(PDO::FETCH_ASSOC);
               <?php }
               else
               {?>
-                <a href="<?php echo $site_url;?>login"  class="w3-btn w3-ripple w3-green mart"><i class="fa fa-plus"></i>Write a review</a>
+                <a href="<?php echo $site_url;?>login" class="w3-btn w3-ripple w3-green mart"><i class="fa fa-plus"></i>Write a review</a>
               <?php }?> 
             </div>
             <div class="col-sm-3 left-po">
               <div id="myProgress">
-                <span class="tp-lo">5 star</span><div id="myBar"></div><span class="rto-lo">0%</span>
+                <span class="tp-lo">5 star</span><div id="myBar" style="width:<?php echo round($rcnt[5]*100/$count2);?>%"></div><span class="rto-lo"><?php echo round($rcnt[5]*100/$count2);?>%</span>
               </div>
 
               <div id="myProgress">
-                <span class="tp-lo">4 star</span><div id="myBar" class="widt-1"></div><span class="rto-lo">0%</span>
+                <span class="tp-lo">4 star</span><div id="myBar" style="width:<?php echo round($rcnt[4]*100/$count2);?>%"></div><span class="rto-lo"><?php echo round($rcnt[4]*100/$count2);?>%</span>
               </div>
 
               <div id="myProgress">
-                <span class="tp-lo">3 star</span><div id="myBar" class="widt-2"></div><span class="rto-lo">0%</span>
+                <span class="tp-lo">3 star</span><div id="myBar" style="width:<?php echo round($rcnt[3]*100/$count2);?>%"></div><span class="rto-lo"><?php echo round($rcnt[3]*100/$count2);?>%</span>
               </div>
 
               <div id="myProgress">
-                <span class="tp-lo">2 star</span><div id="myBar" class="widt-3"></div><span class="rto-lo">0%</span>
+                <span class="tp-lo">2 star</span><div id="myBar" style="width:<?php echo round($rcnt[2]*100/$count2);?>%"></div><span class="rto-lo"><?php echo round($rcnt[2]*100/$count2);?>%</span>
               </div>
 
               <div id="myProgress">
-                <span class="tp-lo">1 star</span><div id="myBar" class="widt-4"></div><span class="rto-lo">0%</span>
+                <span class="tp-lo">1 star</span><div id="myBar" style="width:<?php echo round($rcnt[1]*100/$count2);?>%"></div><span class="rto-lo"><?php echo round($rcnt[1]*100/$count2);?>%</span>
               </div>
             </div>
           </div>
-					
-					<div class="col-sm-6 butten le">
-					  <button class="w3-btn w3-ripple w3-green mat-12">Volunteer</button>
-					</div>
-				</div>	
-			</div>		
-		</div>		
+          
+          <div class="col-sm-6 butten le">
+            <button class="w3-btn w3-ripple w3-green mat-12">Volunteer</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </section>
 <!-- contact end -->
@@ -190,9 +167,7 @@ $( "#dateofplay" ).datepicker({
   changeYear: true,
   yearRange: "-5:+0",
 });
-</script>	
+</script>
 <!-- map end -->
-	
-<?php include('include/footer.php');?>
 
-  
+<?php include('include/footer.php');?>
--- ratingreview.php
+++ ratingreview.php
@@ -4,27 +4,29 @@ session_start();
 
 include('include/config.php');
 
-if(isset($_POST['ratings']))
+if(isset($_POST['ratings']) && !empty($_SESSION['id']))
 {
   if((isset($_REQUEST['rating'])) and (isset($_REQUEST['review']))){
-	  $vid = $_SESSION['id'];
-	  $oid = $_REQUEST['oid'];
-    //$title = $_REQUEST['title'];	
-    $review = $_REQUEST['review'];	
-    $rating = $_REQUEST['rating'];	
-	  $date = date('Y/m/d h:i:sa');
+    $vid = $_SESSION['id'];
+    $oid = $_REQUEST['oid'];
+    //$title = $_REQUEST['title'];
+    $review = $_REQUEST['review'];
+    $rating = $_REQUEST['rating'];
+    $date = date('Y-m-d H:i:s');
 
     $sql = $conn->prepare("INSERT INTO `rating` SET oid=:oid, vid=:vid, rating=:score, review=:description, r_date=:cdate");
     $run = $sql->execute(array(':oid' => $oid , ':vid' => $vid, ':score' => $rating, ':description' => $review, ':cdate' => $date));
     if($run)
     {
-      $_SESSION ['rate'] = 'You have rated '.$rating;	   
-      header("location:" .$_SERVER['HTTP_REFERER']);		 
+      $_SESSION ['rate'] = 'You have rated '.$rating;
+      header("location:" .$_SERVER['HTTP_REFERER']);
     }
   }
   else{
     $_SESSION ['rate'] = 'Fill Ratings';
     header("location:" .$_SERVER['HTTP_REFERER']);
   }
+} else {
+  header("location:" .$_SERVER['HTTP_REFERER']);
 }
 ?>
