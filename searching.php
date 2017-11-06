f5e200e13f75918d1ea0d5be7c7a0f250ab4310d
Laxman Maharjan <email@mlaxman.com.np>
2017-11-06 12:50:16 +0545

    [#13] member profile (connects #13) (#15)

--- searching.php
+++ searching.php
@@ -236,6 +236,12 @@ input.big {
         <div class="col-sm-12">
           <div class="col-sm-3">
             <h1 class="lok-lo"><span style="color:#fff; font-size:35px; font-family:futura-lt-w01-book, sans-serif; letter-spacing:0.15em; text-aline:center;"><b>Collections Development Toolkit</b></span></h1>
+            <div class="sdfr ">
+              <a href="#"><button class="button button2 " style='background-color: hsl(113, 82%, 51%) !important;color: hsl(222, 100%, 34%) !important;'>Resources</button></a>
+              <a href="#"><button class="button button2 " style='background-color: hsl(113, 82%, 51%) !important;color: hsl(222, 100%, 34%) !important;'>Collections</button></a>
+              <button class="button button2 " style='background-color: hsl(113, 82%, 51%) !important;color: hsl(222, 100%, 34%) !important;'>Publishers</button>
+              <a href="members"><button class="button button2 " style='background-color: hsl(113, 82%, 51%) !important;color: hsl(222, 100%, 34%) !important;'>Members</button></a>
+            </div>
           </div>
 
           <div class="col-sm-9">
@@ -259,11 +265,7 @@ input.big {
                   </div>
                   <?php } ?>
 
-                  <div class="sdfr ">
-                    <button class="button button2 " style='background-color: hsl(113, 82%, 51%) !important;color: hsl(222, 100%, 34%) !important;'>Publisher</button>
-                  </div>
-
-                  <div class=" lo-po-12 " >
+                  <div class=" lo-po-12 ">
                     <button type="submit" name ="submit" class="button button2 ba-colo-r"><i class="fa fa-arrow-left" aria-hidden="true" style='margin-left: 0px;float: left;margin-top: 4px;'></i>Select 
                     </button>
                   </div>
